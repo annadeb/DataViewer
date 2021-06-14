4e6626283561a7cfae054575c9a1e729b32f4eaf
Anna Dębowska <anna-debowska@o2.pl>
2021-06-14 15:50:08 +0200

    Wykres roomTemp

--- dataViewer.py
+++ dataViewer.py
@@ -4,12 +4,21 @@
 
 
 from influxdb import DataFrameClient
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
+import numpy as np
 
 client = DataFrameClient(host='127.0.0.1', port=8086, username='root', password='root', database='painMonitor') 
 
 dbs = client.get_list_measurements()
-query='select * from sleepHours WHERE time < \'2021-06-11T08:00:28.000Z\' AND time > \'2021-05-01T06:09:15.000Z\''
+query='select * from roomTemp WHERE time > \'2021-05-17T06:09:15.000Z\' AND time < \'2021-05-17T18:00:28.000Z\''
 
 series = client.query(query,method=u'GET')
 
-print(series["sleepHours"])
+print(series["roomTemp"])
+
+#print(series["roomTemp"].values[0][0])
+#print(series["roomTemp"].index[0])
+
+
+plt.plot(series["roomTemp"].index,series["roomTemp"].values, '-', color='black');
