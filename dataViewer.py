966da0b3d761203b383882ade3cef2a8f8e0c050
Anna Dębowska <anna-debowska@o2.pl>
2021-06-15 23:25:31 +0200

    Wspólna podstawa czasowa, korelacje

--- dataViewer.py
+++ dataViewer.py
@@ -1,22 +1,24 @@
-#from influxdb import InfluxDBClient
-
-#client = InfluxDBClient(host='127.0.0.1', port=8086, database='painMonitor')
-
-
 from influxdb import DataFrameClient
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 import numpy as np
 import pandas as pd
+import scipy.stats
 
 startDate='2021-05-17T06:09:15.000Z'
 endDate='2021-05-17T18:00:28.000Z'
-measurment1='Hr' #'roomTemp';
-measurment2='Temperature';
+measurment1= 'Hr'#'EDA_E4' #'roomCO2'#'Temperature
+measurment2= 'roomTemp'#'nonPersErrorCount'#'roomPress' #'roomNoise' 
+reject_values=False
+
+if measurment1=='Hr' or measurment1=='EDA_E4':
+    reject_values=True
 
 client = DataFrameClient(host='127.0.0.1', port=8086, username='root', password='root', database='painMonitor') 
 
 dbs = client.get_list_measurements()
+
+#pierwsza zmienna
 query='select * from ' + measurment1 + ' WHERE time > \'' + startDate + '\' AND time < \'' + endDate + '\''
 
 series = client.query(query,method=u'GET')
@@ -26,61 +28,77 @@ series = client.query(query,method=u'GET')
 #print(series["roomTemp"].values[0][0])
 #print(series["roomTemp"].index[0])
 
-plt.plot(series[measurment1].index,series[measurment1].values, '-', color='black');
-
+#plt.plot(series[measurment1].index,series[measurment1].values, '-', color='black');
+new_series=series[measurment1].values
+new_series_times=series[measurment1].index
 
 #odrzut skrajnych wartosci - 5%
-seriesvalues = series[measurment1].values.transpose()[0]
-number_largest = max(seriesvalues)
-number_smallest = min(seriesvalues)
-
-series_range = number_largest-number_smallest
-five_percent = 0.05*series_range
-fourty_percent = 0.4*series_range
-
-min_value_accept = number_smallest+five_percent
-max_value_accept = number_largest-fourty_percent
-
-new_series=[]
-new_series_times=[]
-iterate = 0
-for x in seriesvalues:
-    if x <= min_value_accept:
+if reject_values:
+    seriesvalues = series[measurment1].values.transpose()[0]
+    number_largest = max(seriesvalues)
+    number_smallest = min(seriesvalues)
+    
+    series_range = number_largest-number_smallest
+    five_percent = 0.05*series_range
+    fourty_percent = 0.4*series_range
+    
+    min_value_accept = number_smallest+five_percent
+    max_value_accept = number_largest-fourty_percent
+    
+    new_series=[]
+    new_series_times=[]
+    iterate = 0
+    for x in seriesvalues:
+        if x <= min_value_accept:
+            iterate = iterate+1
+            continue
+        if x>= max_value_accept:
+            iterate = iterate+1
+            continue
+        new_series.append(seriesvalues[iterate])
+        new_series_times.append(series[measurment1].index[iterate])
         iterate = iterate+1
-        continue
-    if x>= max_value_accept:
-        iterate = iterate+1
-        continue
-    new_series.append(seriesvalues[iterate])
-    new_series_times.append(series[measurment1].index[iterate])
-    iterate = iterate+1
-
-plt.plot(new_series_times,new_series, '-', color='green');
-
+    
+    #plt.plot(new_series_times,new_series, '-', color='green');
+    
 #srednia kroczaca
-df_series = pd.DataFrame(new_series, index =new_series_times)  
-df_series['average_series'] = df_series.mean(axis=1)
+df_series1 = pd.DataFrame(new_series, index =new_series_times)  
+df_series1['average_series'] = df_series1.mean(axis=1)
+    
+df_series1['SMA_200'] = df_series1.average_series.rolling(200, min_periods=1).mean()
+    
+#plt.plot(df_series1['SMA_200'], '-', color='blue'); 
 
-df_series['SMA_10'] = df_series.average_series.rolling(200, min_periods=1).mean()
+#druga zmienna
+query2='select * from ' + measurment2 + ' WHERE time > \'' + startDate + '\' AND time < \'' + endDate + '\''
+series2 = client.query(query2,method=u'GET')
 
-plt.plot(new_series_times,df_series['SMA_10'], '-', color='blue'); 
+#plt.plot(series2[measurment2].index,series2[measurment2].values, '-', color='red');
 
-#query2='select * from ' + measurment2 + ' WHERE time > \'' + startDate + '\' AND time < \'' + endDate + '\''
-#series2 = client.query(query2,method=u'GET')
+df_series2=pd.DataFrame(series2[measurment2].values, index =series2[measurment2].index.transpose())  
 
-#series2time = pd.Series(series2[measurment2].index.transpose(),series2[measurment2].values.transpose())
-#series2time.resmple('20S')
+#resampling
+series1_resampled = df_series1.resample('15S').bfill()
+series2_resampled = df_series2.resample('15S').bfill()
 
-#plt.plot(series2[measurment2].index,series2[measurment2].values, '-', color='blue');
 
-#iterator = 0
-#arrayElement=0
-#series2resampled = [];
-#for x in series2[measurment2].values:
-#  if iterator%70==0:
-#      series2resampled.append(series2[measurment2].values[iterator][0])
-#      arrayElement=arrayElement+1
-#  iterator=iterator+1
-  
+series1_resampled['Series2']=series2_resampled
 
-  
+#korelacja
+x = series1_resampled['SMA_200']#[0:1144]
+y = series1_resampled['Series2']#[0:1144]
+r = np.corrcoef(x, y)
+#print(r[0, 1])
+#print(r[1, 0])
+
+r2= scipy.stats.pearsonr(x, y)
+print(r2[0])
+         
+r3=scipy.stats.spearmanr(x, y)
+print(r3[0])
+
+r4=scipy.stats.kendalltau(x, y)
+print(r4[0])
+
+plt.plot(x, '-', color='blue'); 
+plt.plot(y, '-', color='green'); 
